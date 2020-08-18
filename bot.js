56e68039bfff9f64460600dbe3f61561fe502dd1
Peter Shortino <petershortino@gmail.com>
2020-08-18 11:33:58 -0500

    test data

--- bot.js
+++ bot.js
@@ -1,3 +1,15 @@
 import Twit from 'twit';
 import  keys from './config.js';
-var T = new Twit(keys);
+const T = new Twit(keys);
+
+
+
+
+T.get('search/tweets', { q: 'rainbow', count:10}, function(err, data, response){
+    console.log(data)
+});
+
+function gotData(err, data, response) { 
+    console.log(gotData);
+};
+console.log(gotData);
