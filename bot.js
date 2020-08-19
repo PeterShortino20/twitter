d09531b173550ab2157f12d4b2760dd2be6992cd
Peter Shortino <petershortino@gmail.com>
2020-08-19 10:54:45 -0500

    post practice

--- bot.js
+++ bot.js
@@ -2,14 +2,25 @@ import Twit from 'twit';
 import  keys from './config.js';
 const T = new Twit(keys);
 
+const tweet = {
+    status: 'Resistance will always exist, make overcoming it a habit'
+}
 
+T.post('statuses/update', tweet, tweeted);
 
+function tweeted(err, data, response){
+    if(err){
+        console.log("Something went wrong");
+    } else {
+        console.log(data);
+    }
+}
 
-T.get('search/tweets', { q: 'rainbow', count:10}, function(err, data, response){
-    console.log(data)
-});
+//T.get('search/tweets', { q: 'rainbow', count:10}, function(err, data, response){
+//    console.log(data)
+//});
 
-function gotData(err, data, response) { 
-    console.log(gotData);
-};
-console.log(gotData);
+//function gotData(err, data, response) { 
+//    console.log(gotData);
+//};
+//console.log(gotData);
