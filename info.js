9e58c90dbcbe500d3402b435b86b59cc927088af
Peter Shortino <petershortino@gmail.com>
2020-08-21 11:21:28 -0500

    a

--- info.js
+++ info.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
 
+
 const array = fs.readFileSync('../pressfield/the-war-of-art.txt').toString();
 const period = array.replace(/[.]/g,".|");
 const exclamation = period.replace(/[!]/g,"!|");
 const question = exclamation.replace(/[?]/g,"?|");
-const clean = question.replace(/[|]/g,"\n");
-const cut = clean.split("\n");
+const clean = question.split("|")
 
-console.log(cut);
+console.log(clean);
