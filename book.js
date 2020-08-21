d7a91e201bec01b5620b626905020ad52c429f5b
Peter Shortino <petershortino@gmail.com>
2020-08-21 18:41:12 -0500

    imports

--- book.js
+++ book.js
@@ -6,6 +6,8 @@ const book = fs.readFileSync('../pressfield/the-war-of-art.txt').toString();
 const period = book.replace(/[.]/g,".|");
 const exclamation = period.replace(/[!]/g,"!|");
 const question = exclamation.replace(/[?]/g,"?|");
-const text = question.split("|")
+const text = question.split("|");
+const amount = text.length
 
-module.exports.text = text;
+
+module.exports.amount = amount;
---
+++ quotes.js
@@ -0,0 +1,5 @@
+const book = require('./book');
+
+num = Math.floor(Math.random() * book.amount);
+console.log(num);
+
