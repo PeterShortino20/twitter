05affddb65f4fe06f15e958cea0572b64394d042
Peter Shortino <petershortino@gmail.com>
2020-08-21 15:56:20 -0500

    launch

--- info.js
+++ info.js
@@ -1,10 +1,11 @@
 const fs = require('fs');
 
+// Read pdf file into variable book, use book variable to replace end of sentence punctuation with a | for easy identification
+//Split the end of every sentence into its own index within the array with all the book text. 
 const book = fs.readFileSync('../pressfield/the-war-of-art.txt').toString();
 const period = book.replace(/[.]/g,".|");
 const exclamation = period.replace(/[!]/g,"!|");
 const question = exclamation.replace(/[?]/g,"?|");
-const clean = question.split("|")
-console.log(clean);
+const text = question.split("|")
 
-module.exports.clean = clean;
+module.exports.text = text;
