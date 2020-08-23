5d27322f23fb8875a2885af360c454d7701ea7ba
Peter Shortino <petershortino@gmail.com>
2020-08-23 11:55:25 -0500

    refactor

--- src/book.js
+++ src/book.js
@@ -3,10 +3,9 @@ const fs = require('fs');
 // Read pdf file into variable book, use book variable to replace end of sentence punctuation with a | for easy identification
 //Split the end of every sentence into its own index within the array with all the book text. 
 const book = fs.readFileSync('../pressfield/the-war-of-art.txt').toString();
-const cleaned = book.replace(/[.]/g,".|").replace(/[!]/g,"!|").replace(/[?]/g,"?|").replace(/[:]/g,"|").replace(/[']/g,"|").replace(/["]/g,"|");
-
-const text = cleaned.split("|");
+const text = book.replace(/[.]/g,".|").replace(/[!]/g,"!|").replace(/[?]/g,"?|").replace(/[:]/g,"|").replace(/[']/g,"|").replace(/["]/g,"|").split("|");
 const amount = text.length;
-//Export for use outside this file
+
+//Export modules for outside use
 module.exports.text = text;
 module.exports.amount = amount;
