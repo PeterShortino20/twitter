b8aa71cfa9520d9d12adeadf20bde21da819bc89
Peter Shortino <petershortino@gmail.com>
2020-08-27 15:42:56 -0500

    .env file

--- src/bot.js
+++ src/bot.js
@@ -1,8 +1,16 @@
 //All imports needed
 const Twit = require('twit');
-const keys = require('./config');
+require('dotenv').config();
+
+const keys = {
+    consumer_key: process.env.CONSUMER_KEY,
+    consumer_secret: process.env.CONSUMER_SECRET,
+    access_token: process.env.ACCESS_TOKEN,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET
+};
 const content = require('./quotes');
 let quotes = content.getQuotes();
+
 
 //Initiate connection to twitter api
 const T = new Twit(keys);
