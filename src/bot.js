768d43819221b1fcfef35fc1e1d86f58dffc98b5
Peter Shortino <petershortino@gmail.com>
2020-08-27 16:03:35 -0500

    changes

--- src/bot.js
+++ src/bot.js
@@ -3,10 +3,10 @@ const Twit = require('twit');
 require('dotenv').config();
 
 const keys = {
-    consumer_key: CONSUMERKEY,
-    consumer_secret: CONSUMER_SECRET,
-    access_token: ACCESS_TOKEN,
-    access_token_secret: ACCESS_TOKEN_SECRET
+    consumer_key: process.env.CONSUMER_KEY,
+    consumer_secret: process.env.CONSUMER_SECRET,
+    access_token: process.env.ACCESS_TOKEN,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET
 };
 const content = require('./quotes');
 let quotes = content.getQuotes();
