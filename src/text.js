643a1584fb5cd0a77a1c508ec37125e3eb7718e3
Peter Shortino <petershortino@gmail.com>
2020-08-27 16:52:39 -0500

    heroku issues

--- src/text.js
+++ src/text.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 // Read pdf file into variable book, use book variable to replace end of sentence punctuation with a | for easy identification
 //Split the end of every sentence into its own index within the array with all the book text. 
-const book = fs.readFileSync('./book_file/woabook.txt').toString();
+const book = fs.readFileSync('../book_file/woabook.txt').toString();
 const text = book.replace(/(\r\n|\n|\r|\f)/gm," ").replace(/[.]/g,".|").replace(/[!]/g,"!|").replace(/[?]/g,"?|").replace(/[:|'|"]/gm,"|").split("|");
 const amount = text.length;
 //Export modules for outside use
