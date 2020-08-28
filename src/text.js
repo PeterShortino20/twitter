86f483bae52130aabd7354d573574e603a1a1d65
Peter Shortino <petershortino@gmail.com>
2020-08-28 11:59:34 -0500

    nodemod

--- src/text.js
+++ src/text.js
@@ -3,7 +3,7 @@ const assert = require('assert');
 
 // Read pdf file into variable book, use book variable to replace end of sentence punctuation with a | for easy identification
 //Split the end of every sentence into its own index within the array with all the book text. 
-const book = fs.readFileSync('~/pressfield/the-war-of-art.txt').toString();
+const book = fs.readFileSync('/the-war-of-art.txt').toString();
 const text = book.replace(/(\r\n|\n|\r|\f)/gm," ").replace(/[.]/g,".|").replace(/[!]/g,"!|").replace(/[?]/g,"?|").replace(/[:|'|"]/gm,"|").split("|");
 const amount = text.length;
 //Export modules for outside use
