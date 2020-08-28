f25714cd87fc4ff34cebac2487e34c8ed7d1458e
Peter Shortino <petershortino@gmail.com>
2020-08-28 12:39:42 -0500

    .

--- src/text.js
+++ src/text.js
@@ -1,9 +1,8 @@
 const fs = require('fs');
-const woa = '/the-war-of-art.txt';
 // Read pdf file into variable book, use book variable to replace end of sentence punctuation with a | for easy identification
 //Split the end of every sentence into its own index within the array with all the book text. 
-//const book = fs.readFileSync('the-war-of-art.txt').toString();
-const text = woa.replace(/(\r\n|\n|\r|\f)/gm," ").replace(/[.]/g,".|").replace(/[!]/g,"!|").replace(/[?]/g,"?|").replace(/[:|'|"]/gm,"|").split("|");
+const book = fs.readFileSync('the-war-of-art.txt').toString();
+const text = book.replace(/(\r\n|\n|\r|\f)/gm," ").replace(/[.]/g,".|").replace(/[!]/g,"!|").replace(/[?]/g,"?|").replace(/[:|'|"]/gm,"|").split("|");
 const amount = text.length;
 //Export modules for outside use
 module.exports.text = text;
