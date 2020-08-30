4c79f6ea7bee38af930fca9e15702c1bc9ca4336
Peter Shortino <petershortino@gmail.com>
2020-08-30 11:18:01 -0500

    update

--- src/text.js
+++ src/text.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 //Input Local file path to txt file generated from your command line pdftextextract command from ReadMe
-const book = fs.readFileSync('~/pressfield/the-war-of-art.txt').toString();
+const book = fs.readFileSync('../the-war-of-art.txt').toString();
 //Makes the text into one long string. Adds additional "|" to punctuation. We then make a sub array of every sentence in the book roughly.
 const text = book.replace(/(\r\n|\n|\r|\f)/gm," ").replace(/[.]/g,".|").replace(/[!]/g,"!|").replace(/[?]/g,"?|").replace(/[:|'|"]/gm,"|").split("|");
 const amount = text.length;
